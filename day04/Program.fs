dedec33514cee78924e754bb2ef9b4eb8e654be9
Löfström Danielsson Tim <tim.lofstrom.danielsson@lm.se>
2024-12-09 19:59:49 +0100

    update

--- day04/Program.fs
+++ day04/Program.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+let readFile (path: string) =
+    let basePath = Directory.GetCurrentDirectory()
+    let filePath = Path.Combine(basePath, path)
+    File.ReadAllLines(filePath)
+
+
+let twoDArray (line: string array)=
+    line
+    |> Array.map (_.ToCharArray())
+    |> array2D
+
+
+let data = readFile "example.txt" |> twoDArray
+
+let indices =
+    data
+    |> Array2D.mapi (fun x y _ -> (x, y))
+    |> Seq.cast<(int * int)>
+    |> Seq.toList
+
+let ex = 1
+let part1 = 1
+let part2 = 1
+
+printfn $"Example: %i{ex}"
+printfn $"Part 1: %i{part1}"
+printfn $"Part 2: %i{part2}"

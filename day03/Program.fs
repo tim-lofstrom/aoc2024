2eb057937f7c3d2dd63147b4dde61f72eb517e1d
Löfström Danielsson Tim <tim.lofstrom.danielsson@lm.se>
2024-12-09 12:51:57 +0100

    update

---
+++ day03/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.IO
+
+let readFile (path: string) =
+    let basePath = Directory.GetCurrentDirectory()
+    let filePath = Path.Combine(basePath, path)
+    File.ReadAllLines(filePath)
+
+let ex = 1
+let part1 = 1
+let part2 = 1
+
+printfn $"Example: %i{ex}"
+printfn $"Part 1: %i{part1}"
+printfn $"Part 2: %i{part2}"
